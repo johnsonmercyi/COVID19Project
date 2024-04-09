3590abdef1a47bf662b1e3bad9be4ed501ce6c0b
johnsonmercyi <johnsonmercy.ng@gmail.com>
2024-04-10 00:21:11 +0100

    made vaccine.js file less busy

--- novaland/js/vaccine.js
+++ novaland/js/vaccine.js
@@ -1,4 +1,4 @@
-import { onThisPageLoad, sidebarMenu } from "./config/nova-settings.mjs";
+import { onThisPageLoad, pagesSubTopics, sidebarMenu } from "./config/nova-settings.mjs";
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
@@ -8,19 +8,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
   const bodyContent = document.querySelector("nova-content");
   const bodyButton = document.querySelector("nova-button");
   const bookAppointmentBtn = bodyButton.shadowRoot.querySelector('button');
-
-  const subTopics = [
-    {
-      header: "Page Subject Header 1",
-      body: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, beatae voluptates eaque blanditiis fugit maxime quos esse odit. Voluptatibus velit voluptatum exercitationem temporibus rem. Cumque maiores a sapiente ducimus obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, beatae voluptates eaque blanditiis fugit maxime quos esse odit. Voluptatibus velit voluptatum exercitationem temporibus rem. Cumque maiores a sapiente ducimus obcaecati.`
-    },{
-      header: "Page Subject Header 2",
-      body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa."
-    },{
-      header: "Page Subject Header 3",
-      body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa."
-    }
-  ]
 
   function initComponent() {
     intro.textheader = `Lorem ipsum dolor sit.`;
@@ -32,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
     pageLink.header = "On This Page";
     pageLink.links = JSON.stringify(onThisPageLoad.vaccine);
 
-    bodyContent.subtopics = JSON.stringify(subTopics);
+    bodyContent.subtopics = JSON.stringify(pagesSubTopics.vaccine);
   }
 
   // Initialize this component
