d271da17b1bcbd1c9ae17254614ed9b35900ae01
johnsonmercyi <johnsonmercy.ng@gmail.com>
2024-04-10 00:11:16 +0100

    Done with the vaccine page

---
+++ novaland/js/config/utility.mjs
@@ -0,0 +1,53 @@
+export const hexToHSL = (hex) => {
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (hex.length === 4) {
+    r = "0x" + hex[1] + hex[1];
+    g = "0x" + hex[2] + hex[2];
+    b = "0x" + hex[3] + hex[3];
+  } else if (hex.length === 7) {
+    r = "0x" + hex[1] + hex[2];
+    g = "0x" + hex[3] + hex[4];
+    b = "0x" + hex[5] + hex[6];
+  }
+
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const cmin = Math.min(r, g, b);
+  const cmax = Math.max(r, g, b);
+  const delta = cmax - cmin;
+  let h = 0;
+  let s = 0;
+  let l = 0;
+
+  if (delta === 0) {
+    h = 0;
+  } else if (cmax === r) {
+    h = ((g - b) / delta) % 6;
+  } else if (cmax === g) {
+    h = (b - r) / delta + 2;
+  } else {
+    h = (r - g) / delta + 4;
+  }
+
+  h = Math.round(h * 60);
+
+  if (h < 0) {
+    h += 360;
+  }
+
+  l = (cmax + cmin) / 2;
+  s = delta === 0 ? 0 : (delta / (1 - Math.abs(2 * l - 1)));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return `hsl(${h}, ${s}%, ${l}%)`;
+};
+
+// Function to calculate the darker color
+export const getDarkerColor = (hslColor) => {
+  const [h, s, l] = hslColor.match(/\d+/g).map(Number);
+  return `hsl(${h}, ${s}%, ${Math.max(l - 5, 0)}%)`;
+};
--- novaland/js/vaccine.js
+++ novaland/js/vaccine.js
@@ -6,14 +6,13 @@ document.addEventListener("DOMContentLoaded", function (e) {
   const sidebar = document.querySelector("nova-sidebar");
   const pageLink = document.querySelector("nova-pagelink");
   const bodyContent = document.querySelector("nova-content");
+  const bodyButton = document.querySelector("nova-button");
+  const bookAppointmentBtn = bodyButton.shadowRoot.querySelector('button');
 
-  // Initialize this component
-  initComponent();
-
-  const content = [
+  const subTopics = [
     {
       header: "Page Subject Header 1",
-      body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa."
+      body: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, beatae voluptates eaque blanditiis fugit maxime quos esse odit. Voluptatibus velit voluptatum exercitationem temporibus rem. Cumque maiores a sapiente ducimus obcaecati.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, beatae voluptates eaque blanditiis fugit maxime quos esse odit. Voluptatibus velit voluptatum exercitationem temporibus rem. Cumque maiores a sapiente ducimus obcaecati.`
     },{
       header: "Page Subject Header 2",
       body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa."
@@ -24,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
   ]
 
   function initComponent() {
-    intro.textheader = `Lorem ipsum dolor sit amet, consectetuer adipiscing elit.`;
+    intro.textheader = `Lorem ipsum dolor sit.`;
     intro.textbody = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.`;
 
     sidebar.link = JSON.stringify(sidebarMenu);
@@ -33,6 +32,14 @@ document.addEventListener("DOMContentLoaded", function (e) {
     pageLink.header = "On This Page";
     pageLink.links = JSON.stringify(onThisPageLoad.vaccine);
 
-    bodyContent.pagesubject = JSON.stringify(content);
+    bodyContent.subtopics = JSON.stringify(subTopics);
   }
+
+  // Initialize this component
+  initComponent();
+
+  bookAppointmentBtn && bookAppointmentBtn.addEventListener("click", () => {
+    alert("You've successfully booked an appointment!");
+  });
+
 });
